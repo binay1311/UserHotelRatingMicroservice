2e30ba810bf44a357298a3b10939e5e11ce781ad
Binay <bose.binaykumar11@gmail.com>
2023-06-29 23:53:30 +0530

    Tried out circuitbreaker, retry and ratelimiter for getSingleUser api in UserController

--- UserService/src/main/java/com/user/service/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/user/service/UserService/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.user.service.UserService.controllers;
 import com.user.service.UserService.entities.User;
 import com.user.service.UserService.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    int retryCount = 1;
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = userService.saveUser(user);
@@ -22,13 +26,20 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = " ")
+//    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallBack")
+//    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallBack")
+    @RateLimiter(name="userRateLimiter", fallbackMethod = "ratingHotelFallBack")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        System.out.println("Retry Count : " + retryCount);
+        retryCount++;
+
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
 
     public ResponseEntity<User> ratingHotelFallBack(String userId, Exception ex){
+        ex.printStackTrace();
+
         User user = User.builder()
                 .userId("12345")
                 .name("dummy")
@@ -36,7 +47,7 @@ public class UserController {
                 .about("This user is created dummy because some service is down")
                 .build();
 
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping
